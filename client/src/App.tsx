f1d9524a7efe5351f56c3ad43f38a48539a310d3
cokerj1 <78478269+cokerj1@users.noreply.github.com>
2024-06-22 01:45:14 +0100

    chore: Update favicon and page title, added basic navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,34 @@
-import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { Dashboard } from './pages/dashboard'
-import { Auth } from './pages/auth'
-import { FinancialRecordsProvider } from './contexts/financial-record-context'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
+import { Auth } from "./pages/auth";
+import { FinancialRecordsProvider } from "./contexts/financial-record-context";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 function App() {
-
   return (
     <Router>
-      <div className='app-container'>
+      <div className="app-container">
+        <div className="navbar">
+          <Link to={"/"}>DashBoard</Link>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
         <Routes>
-          <Route path='/' element={
-            <FinancialRecordsProvider>
-              <Dashboard/>
-          </FinancialRecordsProvider>} />
-          <Route path='auth' element={<Auth/>}/>
+          <Route
+            path="/"
+            element={
+              <FinancialRecordsProvider>
+                <Dashboard />
+              </FinancialRecordsProvider>
+            }
+          />
+          <Route path="auth" element={<Auth />} />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
