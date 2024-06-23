e317a887e3a718ea3f2ce6434232b49c8d281744
cokerj1 <78478269+cokerj1@users.noreply.github.com>
2024-06-23 17:37:32 +0100

    Update .gitignore dependencies, added dotenv for backend environment variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express,{Express} from 'express';
 import mongoose from 'mongoose';
 import financialRecordRouter from "./routes/financial-records";
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app: Express = express();
 // Specifying a port, or defaulting to 3001
@@ -10,12 +11,20 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-const mongoURI: string = "mongodb+srv://jcoker361:2wHykPnb6dWTzn8M@ss0.zkcb7j4.mongodb.net/"
+dotenv.config({
+    path: '.env.local'
+});
+
+const mongoURI: string = process.env.MONGODB_URI || '';
+if (!mongoURI) {
+    console.error('Missing MONGODB_URI in .env.local');
+    process.exit(1);
+}
 
 mongoose
     .connect(mongoURI)
-    .then(()=>console.log("Successful connection to MongoDB!"))
-    .catch((err)=>console.error("Failed to connect to MongoDB: ", err));
+    .then(() => console.log("Successful connection to MongoDB!"))
+    .catch((err) => console.error("Failed to connect to MongoDB: ", err));
 
 app.use("/financial-records", financialRecordRouter);
 
